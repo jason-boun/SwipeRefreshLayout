fc29da5543d0e9f0c350f11805f2c38f524b38d6
jason-boun <jasonboun@163.com>
2017-05-15 12:09:07 +0800

    format code and xml files

--- app/src/main/java/com/zyh/testswiperefresh/MainActivity.java
+++ app/src/main/java/com/zyh/testswiperefresh/MainActivity.java
@@ -7,23 +7,23 @@ import android.view.View.OnClickListener;
 
 public class MainActivity extends FragmentActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				getFragmentManager().beginTransaction().replace(R.id.frameLayout, new MyListFragment()).commit();
-			}
-		});
-		findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				getFragmentManager().beginTransaction().replace(R.id.frameLayout, new MyScrollViewFragment())
-						.commit();
-			}
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.frameLayout, new MyListFragment()).commit();
+            }
+        });
+        findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().replace(R.id.frameLayout, new MyScrollViewFragment())
+                        .commit();
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/zyh/testswiperefresh/MyScrollViewFragment.java
+++ app/src/main/java/com/zyh/testswiperefresh/MyScrollViewFragment.java
@@ -16,35 +16,35 @@ import android.widget.LinearLayout;
 
 public class MyScrollViewFragment extends Fragment {
 
-	private SwipeRefreshLayout		swipeRefreshLayout;
-	private ArrayAdapter<String>	adapter;
-	private LinearLayout					linearlayout;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ArrayAdapter<String> adapter;
+    private LinearLayout linearlayout;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_scroll, container, false);
-		init(v);
-		return v;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_scroll, container, false);
+        init(v);
+        return v;
+    }
 
-	private void init(View v) {
-		linearlayout = (LinearLayout) v.findViewById(R.id.linearlayout);
-		swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeRefreshLayout);
-		swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
-				android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
-		swipeRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
-			@Override
-			public void onRefresh() {
-				new Handler().postDelayed(new Runnable() {
-					public void run() {
-						Button child = new Button(getActivity());
-						child.setText("新添加item："+ new Random().nextInt());
-						linearlayout.addView(child,0);
-						swipeRefreshLayout.setRefreshing(false);
-					}
-				}, 3000);
-			}
-		});
+    private void init(View v) {
+        linearlayout = (LinearLayout) v.findViewById(R.id.linearlayout);
+        swipeRefreshLayout = (SwipeRefreshLayout) v.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
+        swipeRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    public void run() {
+                        Button child = new Button(getActivity());
+                        child.setText("新添加item：" + new Random().nextInt());
+                        linearlayout.addView(child, 0);
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 3000);
+            }
+        });
 
-	}
+    }
 }
